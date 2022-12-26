d7d1cb30b231b6116e1a67f5290997fbf5aebada
Jesus Mata <jesus.mata@lnfdist.com>
2022-12-26 12:02:38 -0600

    Added categories

---
+++ BlazorEcommerce/Server/Data/Config/CategoryConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlazorEcommerce.Server.Data.Config
+{
+    public class CategoryConfig
+    {
+        public CategoryConfig(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Book",
+                    Url = "book"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Movies",
+                    Url = "movies"
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Video Games",
+                    Url = "Video"
+                });
+        }
+    }
+}
--- BlazorEcommerce/Server/Data/DataContext.cs
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -1,44 +1,24 @@
 ﻿
 
+using BlazorEcommerce.Server.Data.Config;
+
 namespace BlazorEcommerce.Server.Data
 {
-    public class DataContext:DbContext
+    public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> opt):base(opt)
+        public DataContext(DbContextOptions<DataContext> opt) : base(opt)
         {
 
         }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Product>().HasData(
-                 new Product
-                 {
-                     Id = 1,
-                     Title = "How to Win Friends and Influence People ",
-                     ImageUrl = "https://th.bing.com/th/id/OIP.pfsMDzqiopN7nHtG78hyzAHaLq?pid=ImgDet&rs=1",
-                     Description = " You can go after the job you want--and get it!\r\nYou can take the job you have--and improve it!\r\nYou can take any situation--and make it work for you! Dale Carnegie's rock-solid, time-tested advice has carried countless people up the ladder of success in their business and personal lives. One of the most groundbreaking and timeless bestsellers of all time, How to Win Friends & Influence People will teach you",
-                     Price = 265.18m
-                 },
-            new Product
-            {
-                Id = 2,
-                Title = "The 48 Laws of Power ",
-                ImageUrl = "https://thepowermoves.com/wp-content/uploads/the-48-laws-of-power-list-712x1024.jpg",
-                Description = "Amoral, cunning, ruthless, and instructive, this multi-million-copy New York Times bestseller is the definitive manual for anyone interested in gaining, observing, or defending against ultimate control – from the author of The Laws of Human Nature.",
-                Price = 292.96m
-            },
-            new Product
-            {
-                Id = 3,
-                Title = "Atomic Habits",
-                ImageUrl = "https://th.bing.com/th/id/OIP.e6TtDwNcDhX79yDamKZq9wHaJa?pid=ImgDet&rs=1",
-                Description = "No importa sus metas, Atomic Habits este libro en inglés ofrece un marco probado para mejorar... cada día. James Clear, uno de los principales expertos del mundo en formación de hábitos, revela estrategias prácticas que le enseñarán exactamente cómo formar buenos hábitos, romper los malos y dominar los pequeños comportamientos que conducen a resultados notables.",
-                Price = 421.30m
-            }); 
 
-            base.OnModelCreating(modelBuilder);
+            new ProductConfig(modelBuilder.Entity<Product>());
+            new CategoryConfig(modelBuilder.Entity<Category>());
+
         }
     }
 }
